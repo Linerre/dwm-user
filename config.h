102087273a3f2a6a4be5f43b37b2e208791d15a9
Linerre <leon.errelin@outlook.com>
2021-02-17 17:13:15 +0800

    release: version 1.0.0

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ typedef struct {
 } Sp;
 const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
 const char *spcmd2[] = {"st", "-n", "spfm", "-g", "144x41", "-e", "ranger", NULL };
-const char *spcmd3[] = {"keepassxc", NULL };
+/*const char *spcmd3[] = {"keepassxc", NULL };*/
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",      spcmd1},
